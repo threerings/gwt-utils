193aa58dc34a852a02ae61982ec8f3d3bbea94bb
mdb <mdb@aabf2320-13c4-7ad8-78c9-eecaf4a0dffc>
2009-07-13 04:27:00 +0000

    Moved the confirmation code in from Everything. Yay for reusable code.

--- trunk/src/java/com/threerings/gwt/util/ClickCallback.java
+++ trunk/src/java/com/threerings/gwt/util/ClickCallback.java
@@ -8,13 +8,17 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.gwt.ui.EnterClickAdapter;
 import com.threerings.gwt.ui.Popups;
+import com.threerings.gwt.ui.SmartTable;
 
 /**
  * Allows one to wire up a button and a service call into one concisely specified little chunk of
@@ -77,26 +81,6 @@ public abstract class ClickCallback<T>
     protected abstract boolean gotResult (T result);
 
     /**
-     * Reports a failure to the user. Derived classes can override this and customize the way
-     * failure is reported if they so desire.
-     */
-    protected void reportFailure (Throwable cause)
-    {
-        Widget near = _onEnter;
-        if (near == null && _trigger instanceof Widget) {
-            near = (Widget)_trigger;
-        }
-        if (_onEnter != null) {
-            _onEnter.setFocus(true);
-        }
-        if (near == null) {
-            Popups.error(formatError(cause));
-        } else {
-            Popups.errorNear(formatError(cause), near);
-        }
-    }
-
-    /**
      * Formats the error indicated by the supplied throwable. The default implementation simply
      * returns {@link Throwable#getMessage}.
      */
@@ -105,11 +89,68 @@ public abstract class ClickCallback<T>
         return cause.getMessage();
     }
 
-    protected void takeAction ()
+    /**
+     * If a callback wishes to require confirmation it can override this method and return a
+     * message that will be displayed to confirm the action before it is taken.
+     */
+    protected String getConfirmMessage ()
     {
-        if (callService()) {
-            setEnabled(false);
+        return null;
+    }
+
+    /**
+     * Returns the choices given to the user when confirming the callback. The default choices on
+     * the confirm dialog are "No", "Yes".
+     */
+    protected String[] getConfirmChoices ()
+    {
+        return new String[] { "No", "Yes" };
+    }
+
+    /**
+     * Override this method and return true if you wish your confirm message to be interpreted as
+     * HTML. Be careful!
+     */
+    protected boolean confirmMessageIsHTML ()
+    {
+        return false;
+    }
+
+    protected void takeAction (boolean confirmed)
+    {
+        // if we have no confirmation message or are already confirmed, do the deed
+        String confmsg = getConfirmMessage();
+        if (confirmed || confmsg == null) {
+            if (callService()) {
+                setEnabled(false);
+            }
+            return;
         }
+
+        // otherwise display a confirmation panel
+        final PopupPanel confirm = new PopupPanel();
+        confirm.setStyleName("gwt-ConfirmPopup");
+        SmartTable contents = new SmartTable(5, 0);
+        if (confirmMessageIsHTML()) {
+            contents.setHTML(0, 0, confmsg, 2, "Message");
+        } else {
+            contents.setText(0, 0, confmsg, 2, "Message");
+        }
+        String[] choices = getConfirmChoices();
+        contents.setWidget(1, 0, new Button(choices[0], new ClickHandler() {
+            public void onClick (ClickEvent event) {
+                confirm.hide(); // abort!
+            }
+        }));
+        contents.setWidget(1, 1, new Button(choices[1], new ClickHandler() {
+            public void onClick (ClickEvent event) {
+                confirm.hide();
+                takeAction(true);
+            }
+        }));
+        contents.getFlexCellFormatter().setHorizontalAlignment(1, 1, HasAlignment.ALIGN_RIGHT);
+        confirm.setWidget(contents);
+        confirm.center();
     }
 
     protected void setEnabled (boolean enabled)
@@ -142,9 +183,25 @@ public abstract class ClickCallback<T>
         }
     }
 
+    protected void reportFailure (Throwable cause)
+    {
+        Widget near = _onEnter;
+        if (near == null && _trigger instanceof Widget) {
+            near = (Widget)_trigger;
+        }
+        if (_onEnter != null) {
+            _onEnter.setFocus(true);
+        }
+        if (near == null) {
+            Popups.error(formatError(cause));
+        } else {
+            Popups.errorNear(formatError(cause), near);
+        }
+    }
+
     protected ClickHandler _onClick = new ClickHandler() {
         public void onClick (ClickEvent event) {
-            takeAction();
+            takeAction(false);
         }
     };
 
