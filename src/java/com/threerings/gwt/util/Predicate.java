11de784428c6d3d1a94bdd3f40bcdf651d6ae576
ray <ray@aabf2320-13c4-7ad8-78c9-eecaf4a0dffc>
2009-08-20 00:17:19 +0000

    Added a note to keep me from wasting time in the future. Shut up and write code, I guess, maintenance is for losers.

--- src/java/com/threerings/gwt/util/Predicate.java
+++ src/java/com/threerings/gwt/util/Predicate.java
@@ -5,6 +5,16 @@ package com.threerings.gwt.util;
 
 /**
  * A single argument predicate function.
+ *
+ * TODO: google-collect is supposed to work with GWT, especially simple fucking shit like
+ * a stupid fucking INTERFACE, but nobody seems to yet be building a GWT-compatible jar and
+ * I tried for a while, but ran up against the problems with needing the source in the same jar
+ * for the standard Java annotations. And now I have a tension headache.
+ *
+ * This note is here to remind you not to try this unless you really know what you're doing,
+ * or they actually distribute a GWT module for adapting the google collect code.
+ * Actually, I'm sure I could just copy the code in here, and get it working that way,
+ * but that's ugly and not easily maintainable.
  */
 public interface Predicate<T>
 {
