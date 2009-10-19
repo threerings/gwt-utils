0eb9b845da1e446da436696019c2e3b455982dbd
jamie <jamie@aabf2320-13c4-7ad8-78c9-eecaf4a0dffc>
2009-10-19 19:41:42 +0000

    New cell mutator nested class to make smart table cell manipulation more legible. Inspired by the incredibly verbose _table.getFlexCellFormatter(row, col).setVerticalAlignment(HasVerticalAlignment.ALIGN_TOP), which becomes _table.cell(row, col).alignTop(). Other things are a bit easier too, like being able to set the style without passing in a col span of 1. Also, more methods can be added such as rowSpan or clear(). The old methods could be taken out now, but I've left them in because this is such a widely used class.

--- trunk/src/java/com/threerings/gwt/ui/SmartTable.java
+++ trunk/src/java/com/threerings/gwt/ui/SmartTable.java
@@ -4,6 +4,8 @@
 package com.threerings.gwt.ui;
 
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -11,6 +13,129 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class SmartTable extends FlexTable
 {
+    /**
+     * Performs a number of convenient modifications on a cell, each returning the mutator instance
+     * for very legible chaining of modifications.
+     */
+    public class CellMutator
+    {
+        /**
+         * Sets the cell content to the given widget.
+         */
+        public CellMutator widget (Widget widget)
+        {
+            setWidget(_row, _col, widget);
+            return this;
+        }
+
+        /**
+         * Sets the cell content to the given text.
+         */
+        public CellMutator text (String text)
+        {
+            setText(_row, _col, text);
+            return this;
+        }
+
+        /**
+         * Sets the cell content to the given html.
+         */
+        public CellMutator html (String html)
+        {
+            setHTML(_row, _col, html);
+            return this;
+        }
+
+        /**
+         * Sets the colspan attribute of the cell to the given span.
+         */
+        public CellMutator colSpan (int span)
+        {
+            getFlexCellFormatter().setColSpan(_row, _col, span);
+            return this;
+        }
+
+        /**
+         * Adds the given styles to the cell.
+         */
+        public CellMutator styles (String... names)
+        {
+            setStyleNames(_row, _col, names);
+            return this;
+        }
+
+        /**
+         * Sets the vertical alignment for the cell to "top".
+         */
+        public CellMutator alignTop ()
+        {
+            valign(_row, _col, HasVerticalAlignment.ALIGN_TOP);
+            return this;
+        }
+
+        /**
+         * Sets the vertical alignment for the cell to "middle".
+         */
+        public CellMutator alignMiddle ()
+        {
+            valign(_row, _col, HasVerticalAlignment.ALIGN_MIDDLE);
+            return this;
+        }
+
+        /**
+         * Sets the vertical alignment for the cell to "bottom".
+         */
+        public CellMutator alignBottom ()
+        {
+            valign(_row, _col, HasVerticalAlignment.ALIGN_BOTTOM);
+            return this;
+        }
+
+        /**
+         * Sets the horizontal alignment for the cell to "left".
+         */
+        public CellMutator alignLeft ()
+        {
+            halign(_row, _col, HasHorizontalAlignment.ALIGN_LEFT);
+            return this;
+        }
+
+        /**
+         * Sets the horizontal alignment for the cell to "center".
+         */
+        public CellMutator alignCenter ()
+        {
+            halign(_row, _col, HasHorizontalAlignment.ALIGN_CENTER);
+            return this;
+        }
+
+        /**
+         * Sets the horizontal alignment for the cell to "right".
+         */
+        public CellMutator alignRight ()
+        {
+            halign(_row, _col, HasHorizontalAlignment.ALIGN_RIGHT);
+            return this;
+        }
+
+        /**
+         * Sets the horizontal alignment for the cell to "default".
+         */
+        public CellMutator alignDefault ()
+        {
+            halign(_row, _col, HasHorizontalAlignment.ALIGN_DEFAULT);
+            return this;
+        }
+
+        protected CellMutator (int row, int col)
+        {
+            _row = row;
+            _col = col;
+        }
+
+        protected int _row, _col;
+    }
+
     public SmartTable ()
     {
     }
@@ -108,5 +233,25 @@ public class SmartTable extends FlexTable
                 getFlexCellFormatter().addStyleName(row, column, style);
             }
         }
+    }
+
+    /**
+     * Gets a mutator for the given cell.
+     */
+    public CellMutator cell (int row, int col)
+    {
+        return new CellMutator(row, col);
+    }
+
+    protected void valign (
+        int row, int col, HasVerticalAlignment.VerticalAlignmentConstant align)
+    {
+        getFlexCellFormatter().setVerticalAlignment(row, col, align);
+    }
+
+    protected void halign (
+        int row, int col, HasHorizontalAlignment.HorizontalAlignmentConstant align)
+    {
+        getFlexCellFormatter().setHorizontalAlignment(row, col, align);
     }
 }
