51b13612766fc168665445b74ff48e41f2e8e228
samskivert <samskivert@aabf2320-13c4-7ad8-78c9-eecaf4a0dffc>
2010-10-08 16:39:13 +0000

    Added various other scroll related methods.

--- trunk/src/main/java/com/threerings/gwt/util/WindowUtil.java
+++ trunk/src/main/java/com/threerings/gwt/util/WindowUtil.java
@@ -48,4 +48,54 @@ public class WindowUtil
     {
         Window.scrollTo(Window.getScrollLeft(), target.getElement().getAbsoluteTop());
     }
+
+    /**
+     * Returns the vertical scroll position needed to place the specified target widget in view,
+     * while trying to minimize scrolling.
+     */
+    public static int getScrollIntoView (Widget target)
+    {
+        int top = Window.getScrollTop(), height = Window.getClientHeight();
+        int ttop = target.getElement().getAbsoluteTop();
+        int theight = target.getElement().getClientHeight();
+        // if the target widget is taller than the browser window, or is above the current scroll
+        // position of the browser window, scroll the top of the widget to the top of the window
+        if (theight > height || ttop < top) {
+            return ttop;
+        // otherwise scroll the bottom of the widget to the bottom of the window
+        } else if (ttop + theight > top + height) {
+            return ttop - (height - theight);
+        } else {
+            return top; // no scrolling needed
+        }
+    }
+
+    /**
+     * Scrolls the window to place the specified target widget in view, while trying to minimize
+     * scrolling.
+     */
+    public static void scrollIntoView (Widget target)
+    {
+        Window.scrollTo(Window.getScrollLeft(), getScrollIntoView(target));
+    }
+
+    /**
+     * Returns the vertical scroll position needed to center the target widget vertically in the
+     * browser viewport. If the widget is taller than the viewport, its top is returned.
+     */
+    public static int getScrollToMiddle (Widget target)
+    {
+        int wheight = Window.getClientHeight(), theight = target.getElement().getClientHeight();
+        int ttop = target.getElement().getAbsoluteTop();
+        return Math.max(ttop, ttop + (wheight - theight));
+    }
+
+    /**
+     * Centers the target widget vertically in the browser viewport. If the widget is taller than
+     * the viewport, its top is aligned with the top of the viewport.
+     */
+    public static void scrollToMiddle (Widget target)
+    {
+        Window.scrollTo(Window.getScrollLeft(), getScrollToMiddle(target));
+    }
 }
