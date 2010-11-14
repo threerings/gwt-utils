04184408901624f75b52e53b131bbcb02ae2b498
samskivert <samskivert@aabf2320-13c4-7ad8-78c9-eecaf4a0dffc>
2010-11-14 02:41:49 +0000

    Added Popups.Position and support for positioning ABOVE as well as OVER and BELOW, which we already supported in an ad hoc manner.
    Deprecated xxxNear() in favor of xxxBelow(). Added xxxAbove() where
    appropriate.
    
    Added binding of poppedness to a boolean value, and similar with deferred
    creation of the popup.

--- trunk/src/main/java/com/threerings/gwt/ui/Popups.java
+++ trunk/src/main/java/com/threerings/gwt/ui/Popups.java
@@ -23,27 +23,39 @@ package com.threerings.gwt.ui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+import com.threerings.gwt.util.Preconditions;
+import com.threerings.gwt.util.Value;
 
 /**
  * Popup-related utility methods.
  */
 public class Popups
 {
+    /** Used to defer the creation of a popup panel. */
+    public interface Thunk {
+        /** Creates and returns a deferred popup panel. */
+        PopupPanel createPopup ();
+    }
+
+    /** Positions supported by {@link #show} and other methods. */
+    public enum Position { ABOVE, OVER, BELOW };
+
     /**
-     * Displays an info message.
+     * Displays an info message centered in the browser window.
      */
     public static void info (String message)
     {
         new InfoPopup(message).showCentered();
     }
 
-    /**
-     * Displays an info message near the specified widget.
-     */
+    /** @deprecated Use {@link #infoBelow} */ @Deprecated
     public static void infoNear (String message, Widget target)
     {
         new InfoPopup(message).showNear(target);
@@ -59,6 +71,14 @@ public class Popups
     }
 
     /**
+     * Displays an info message below the specified widget.
+     */
+    public static void infoBelow (String message, Widget target)
+    {
+        show(new InfoPopup(message), Position.BELOW, target);
+    }
+
+    /**
      * Displays error feedback to the user in a non-offensive way.
      */
     public static void error (String message)
@@ -66,10 +86,7 @@ public class Popups
         new InfoPopup(message).toError().showCentered();
     }
 
-    /**
-     * Displays error feedback to the user in a non-offensive way. The error feedback is displayed
-     * near the supplied component and if the component supports focus, it is focused.
-     */
+    /** @deprecated Use {@link #errorBelow} */ @Deprecated
     public static void errorNear (String message, Widget source)
     {
         if (source instanceof FocusWidget) {
@@ -79,18 +96,58 @@ public class Popups
     }
 
     /**
-     * Shows the supplied popup panel near the specified target.
+     * Displays error feedback to the user in a non-offensive way. The error feedback is displayed
+     * near the supplied component and if the component supports focus, it is focused.
      */
-    public static void showNear (PopupPanel popup, Widget target)
+    public static void errorBelow (String message, Widget source)
+    {
+        if (source instanceof FocusWidget) {
+            ((FocusWidget)source).setFocus(true);
+        }
+        show(new InfoPopup(message).toError(), Position.BELOW, source);
+    }
+
+    /**
+     * Shows the supplied popup in the specified position relative to the specified target widget.
+     */
+    public static void show (PopupPanel popup, Position pos, Widget target)
     {
         popup.setVisible(false);
         popup.show();
-        int left = target.getAbsoluteLeft();
+
+        int top, left = target.getAbsoluteLeft();
         if (left + popup.getOffsetWidth() > Window.getClientWidth()) {
             left = Math.max(0, Window.getClientWidth() - popup.getOffsetWidth());
         }
-        popup.setPopupPosition(left, target.getAbsoluteTop() + target.getOffsetHeight() + NEAR_GAP);
+        switch (pos) {
+        case ABOVE:
+            top = target.getAbsoluteTop() - popup.getOffsetHeight() - NEAR_GAP;
+            break;
+        case OVER:
+            top = target.getAbsoluteTop();
+            break;
+        default:
+        case BELOW:
+            top = target.getAbsoluteTop() + target.getOffsetHeight() + NEAR_GAP;
+            break;
+        }
+
+        popup.setPopupPosition(left, top);
         popup.setVisible(true);
+    }
+
+    /** @deprecated Use {@link #showBelow} */ @Deprecated
+    public static void showNear (PopupPanel popup, Widget target)
+    {
+        show(popup, Position.BELOW, target);
+    }
+
+    /**
+     * Shows the supplied popup panel near the specified target.
+     */
+    public static void showAbove (PopupPanel popup, Widget target)
+    {
+        show(popup, Position.ABOVE, target);
     }
 
     /**
@@ -98,8 +155,15 @@ public class Popups
      */
     public static void showOver (PopupPanel popup, Widget target)
     {
-        popup.setPopupPosition(target.getAbsoluteLeft(), target.getAbsoluteTop());
-        popup.show();
+        show(popup, Position.OVER, target);
+    }
+
+    /**
+     * Shows the supplied popup panel below the specified target.
+     */
+    public static void showBelow (PopupPanel popup, Widget target)
+    {
+        show(popup, Position.BELOW, target);
     }
 
     /**
@@ -176,6 +240,92 @@ public class Popups
                 popup.hide();
             }
         };
+    }
+
+    /**
+     * Binds the popped up state of a popup to the supplied boolean value and vice versa (i.e. if
+     * the popup is popped down, the value will be updated to false). The popup is not created
+     * until the first time the value is true.
+     */
+    public static void bindPopped (final Value<Boolean> popped, final Thunk thunk)
+    {
+        Preconditions.checkNotNull(thunk, "thunk");
+        popped.addListenerAndTrigger(new Value.Listener<Boolean>() {
+            public void valueChanged (Boolean visible) {
+                if (visible) {
+                    popped.removeListener(this);
+                    bindPopped(popped, thunk.createPopup());
+                }
+            }
+        });
+    }
+
+    /**
+     * Binds the popped up state of a popup to the supplied boolean value and vice versa (i.e. if
+     * the popup is popped down, the value will be updated to false).
+     */
+    public static void bindPopped (final Value<Boolean> popped, final PopupPanel panel)
+    {
+        Preconditions.checkNotNull(panel, "panel");
+        panel.addCloseHandler(new CloseHandler<PopupPanel>() {
+            public void onClose (CloseEvent<PopupPanel> event) {
+                popped.update(false);
+            }
+        });
+        popped.addListenerAndTrigger(new Value.Listener<Boolean>() {
+            public void valueChanged (Boolean visible) {
+                if (visible) {
+                    panel.center();
+                } else {
+                    panel.hide();
+                }
+            }
+        });
+    }
+
+    /**
+     * Binds the popped up state of a popup to the supplied boolean value and vice versa (i.e. if
+     * the popup is popped down, the value will be updated to false). The popup is not created
+     * until the first time the value is true.
+     */
+    public static void bindPopped (final Value<Boolean> popped, final Position pos,
+                                   final Widget target, final Thunk thunk)
+    {
+        Preconditions.checkNotNull(target, "target");
+        Preconditions.checkNotNull(thunk, "thunk");
+        popped.addListenerAndTrigger(new Value.Listener<Boolean>() {
+            public void valueChanged (Boolean visible) {
+                if (visible) {
+                    popped.removeListener(this);
+                    bindPopped(popped, pos, target, thunk.createPopup());
+                }
+            }
+        });
+    }
+
+    /**
+     * Binds the popped up state of a popup to the supplied boolean value and vice versa (i.e. if
+     * the popup is popped down, the value will be updated to false).
+     */
+    public static void bindPopped (final Value<Boolean> popped, final Position pos,
+                                   final Widget target, final PopupPanel panel)
+    {
+        Preconditions.checkNotNull(target, "target");
+        Preconditions.checkNotNull(panel, "panel");
+        panel.addCloseHandler(new CloseHandler<PopupPanel>() {
+            public void onClose (CloseEvent<PopupPanel> event) {
+                popped.update(false);
+            }
+        });
+        popped.addListenerAndTrigger(new Value.Listener<Boolean>() {
+            public void valueChanged (Boolean visible) {
+                if (visible) {
+                    show(panel, pos, target);
+                } else {
+                    panel.hide();
+                }
+            }
+        });
     }
 
     protected static final int NEAR_GAP = 5;
